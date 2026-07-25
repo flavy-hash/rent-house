9f70e545d435d40b517dd8eee4138cfb57442d55
flavy-hash <flavianmichael663@gmail.com>
2026-07-25 14:04:49 +0300

    modfying the routes web

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PropertyController::class, 'home'])->name('home');
 
 Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
-Route::get('/properties/list', [PropertyController::class, 'create'])->name('properties.create');
-Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
+
+// Saved / favourites (browser-based, no login needed)
+Route::get('/saved', [PropertyController::class, 'saved'])->name('saved');
+Route::get('/saved/cards', [PropertyController::class, 'savedCards'])->name('saved.cards');
+
 Route::get('/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
+
+// Landlords list & manage properties through the Filament panel at /admin.
